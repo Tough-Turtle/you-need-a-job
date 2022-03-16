c9b0e3ffb94eba70b064bd2fcbf5bb6b935aee76
Alex Hersler <ahersler@gmail.com>
2022-03-16 11:39:13 -0700

    complete /user patch to update status. using dummy data on server

--- client/components/JobSearchResult.jsx
+++ client/components/JobSearchResult.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 // import heart from '../../assets/icons/heart.svg'
 // company: "Big Sky Senior Living on Waterford Way"
@@ -31,29 +32,29 @@ export const JobSearchResult = props => {
     // console.log(e.target.parentElement.parentElement)
 
     //   const { title, summary, url, company, postDate, salary, isEasyApply, user } = req.body;
-    const data = JSON.stringify({
+
+    //  url: e.target.parentElement.previousSibling.lastChild.innerText
+
+    const data = {
       title,
       summary,
-      url: e.target.parentElement.previousSibling.lastChild.innerText,
+      url,
       company,
       postDate,
       salary,
       isEasyApply,
       user: props.username,
-    });
+    };
 
     // title, summary, url, company, postDate, salary, isEasyApply, user
 
     console.log('body data in handle select favorite', data);
     // {"title":"Software Engineer","summary":"Proven ability to work through deadlines and meet time constraints.\n Working knowledge of software development methods and processes including Waterfall and…","url":"https://www.indeed.com/company/Calypso-Way/jobs/Software-Engineer-3575a6f7d9557738?fccid=fc22f3e3e1931727&vjs=3","company":"Calypso Way","postDate":"PostedToday","salary":"$45 - $50 an hourFull-time +1","isEasyApply":true,"user":"Hong"}
+
     if (!favorited) {
-      fetch('http://localhost:3001/user', {
-        // mode: 'no-cors',
+      axios({
         method: 'POST',
-        header: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-        },
+        url: 'http://localhost:3001/user',
         body: data,
       })
         .then(res => res.json())
@@ -64,20 +65,41 @@ export const JobSearchResult = props => {
         })
         .catch(err => console.log('ERROR: ', err));
     } else {
-      // fetch('http://localhost:3001/user', {
-      //   method: 'DELETE',
-      //   header: {
-      //     'Access-Control-Allow-Origin': '*',
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: data,
-      // })
-      //   .then(res => res.json())
-      //   .then(data => {
-      //     console.log('got stuff back');
-      //     console.log(data);
       setFavorited(false);
     }
+
+    // if (!favorited) {
+    //   fetch('http://localhost:3001/user', {
+    //     // mode: 'no-cors',
+    //     method: 'POST',
+    //     header: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: data,
+    //   })
+    //     .then(res => res.json())
+    //     .then(data => {
+    //       console.log('got stuff back');
+    //       console.log(data);
+    //       setFavorited(true);
+    //     })
+    //     .catch(err => console.log('ERROR: ', err));
+    // } else {
+    //   // fetch('http://localhost:3001/user', {
+    //   //   method: 'DELETE',
+    //   //   header: {
+    //   //     'Access-Control-Allow-Origin': '*',
+    //   //     'Content-Type': 'application/json',
+    //   //   },
+    //   //   body: data,
+    //   // })
+    //   //   .then(res => res.json())
+    //   //   .then(data => {
+    //   //     console.log('got stuff back');
+    //   //     console.log(data);
+    //   setFavorited(false);
+    // }
   };
   return (
     <div className="search-result">
--- server/server.js
+++ server/server.js
@@ -96,40 +96,40 @@ app.get('/search', indeedController.search, (req, res) => {
 // return list of liked jobs
 app.get('/user', userController.getLiked, (req, res) => {
   console.log('final user get middleware');
-  res.status(200).header('Access-Control-Allow-Origin', '*').json(res.locals.liked);
-  // res
-  //   .status(200)
-  //   .header('Access-Control-Allow-Origin', '*')
-  //   .json([
-  //     {
-  //       _id: 1,
-  //       postDate: 'just Posted',
-  //       title: 'Software Engineer',
-  //       company: 'Google',
-  //       isEasyApply: false,
-  //       salary: '$200,000',
-  //       url: 'indeed.com/greatgooglejob',
-  //       location: 'Mountain View, CA',
-  //       summary: 'React whiz needed.',
-  //       note: 'TBD',
-  //       date_apply: undefined,
-  //       status: 'Not Applied',
-  //     },
-  //     {
-  //       _id: 2,
-  //       postDate: 'post yesterday',
-  //       title: 'Software Developer',
-  //       company: 'Netflix',
-  //       isEasyApply: false,
-  //       salary: '$180,000',
-  //       url: 'indeed.com/greatnextflixjob',
-  //       location: 'Los Angeles, CA',
-  //       summary: 'Redux whiz needed.',
-  //       note: 'TBD',
-  //       date_apply: '3/10/22',
-  //       status: 'Onsite',
-  //     },
-  //   ]);
+  // res.status(200).header('Access-Control-Allow-Origin', '*').json(res.locals.liked);
+  res
+    .status(200)
+    .header('Access-Control-Allow-Origin', '*')
+    .json([
+      {
+        _id: 1,
+        postDate: 'just Posted',
+        title: 'Software Engineer',
+        company: 'Google',
+        isEasyApply: false,
+        salary: '$200,000',
+        url: 'indeed.com/greatgooglejob',
+        location: 'Mountain View, CA',
+        summary: 'React whiz needed.',
+        note: 'TBD',
+        date_apply: undefined,
+        status: 'Not Applied',
+      },
+      {
+        _id: 2,
+        postDate: 'post yesterday',
+        title: 'Software Developer',
+        company: 'Netflix',
+        isEasyApply: false,
+        salary: '$180,000',
+        url: 'indeed.com/greatnextflixjob',
+        location: 'Los Angeles, CA',
+        summary: 'Redux whiz needed.',
+        note: 'TBD',
+        date_apply: '3/10/22',
+        status: 'Onsite',
+      },
+    ]);
 });
 
 // add a liked job posting for a user
